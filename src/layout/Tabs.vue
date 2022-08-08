7bb09ada41e400a2cede31f045e885e24715d2ff
Fan223 <fanj.zh@foxmail.com>
2022-08-08 17:29:54 +0800

    修改标签页样式

--- src/layout/Tabs.vue
+++ src/layout/Tabs.vue
@@ -65,3 +65,30 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+/*导航条下方边框*/
+/deep/ .el-tabs__nav-scroll{
+  border-bottom: 1px solid #000000;
+  box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.15);
+}
+/* 导航条下方边框阴影*/
+/deep/ .el-tabs__nav-wrap {
+  box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.15) ;
+}
+/*切换项下面的长条，控制其长度*/
+/deep/ .el-tabs__active-bar.is-top{
+  text-align: center;
+  width: 0px !important;      /* 不加important  不会生效，important会覆盖行内样式权限级别*/
+  padding: 0 50px !important; /* 控制位置*/
+  left:8.5%;                  /* 根据实际情设置，尽量居中就行*/
+  box-sizing: border-box;
+  background-clip: content-box;
+}
+/deep/ .el-tabs__item.is-active{    /* 切换活动项的文字颜色 */
+color: #000000 !important;
+}
+/deep/ .el-tabs__active-bar{
+  background-color: #229922 !important;   /* 切换活动项的长条颜色 */
+}
+</style>
